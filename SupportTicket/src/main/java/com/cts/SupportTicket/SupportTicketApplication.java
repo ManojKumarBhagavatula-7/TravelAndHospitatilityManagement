3d4b560193d22d948a2528f8899aaa748761868f
Manoj Kumar Bhagavatula <SaiRamManojKumar.Bhagavatula@cognizant.com>
2025-06-23 15:27:37 +0530

    add support ticket

---
+++ SupportTicket/src/main/java/com/cts/SupportTicket/SupportTicketApplication.java
@@ -0,0 +1,13 @@
+package com.cts.SupportTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportTicketApplication.class, args);
+	}
+
+}
